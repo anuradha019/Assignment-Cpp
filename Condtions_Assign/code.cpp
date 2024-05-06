5bba660c26addca2a6dd94b0357162223450b3d5
anuradha-022 <annumangrati22@gmail.com>
2024-05-06 21:55:15 +0530

    assignment complete execpt armstrong

---
+++ Condtions_Assign/code.cpp
@@ -0,0 +1,37 @@
+// get a number from the user and print whether it's positive, negative or zero
+
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     int a;
+//     cout << "Enter number : ";
+//     cin >> a;
+
+//     if(a > 0) {
+//         cout << "Positive Number" << endl;
+//     }else if (a < 0){
+//         cout << "Negative Number" << endl;
+//     }else {
+//         cout << " Zero " <<  endl;
+//     }
+//     return 0;
+// }
+
+// find leap year
+
+// int main() {
+//     int year;
+//     cout << "Enter a year :";
+//     cin >> year;
+
+//    if((year % 4 == 0) || (year % 400 == 0)) {
+//       cout << "It is a Leap Year" << endl;
+//    }else {
+//       cout << "It is not a leap Year" << endl;
+//    }
+//    return 0;
+// }
+
+
+
